86562ed095575e11b433d8ebc552d647664e37c5
Antony Jay <antony.jay@workday.com>
2021-12-03 22:41:09 -0800

    Minor updates

--- src/main/java/com/jantony/concurrency/Timer.java
+++ src/main/java/com/jantony/concurrency/Timer.java
@@ -1,5 +1,6 @@
 package com.jantony.concurrency;
 
+import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.stream.IntStream;
@@ -7,6 +8,32 @@ import java.util.stream.IntStream;
 public class Timer {
     private BlockingQueue<Task> taskQueue = new PriorityBlockingQueue<>();
     private Object lock = new Object();
+    private ArrayList<Thread> workerThreads = new ArrayList<>();
+
+    public Timer(int parallel) {
+        IntStream.range(0, parallel).forEach(x ->
+                {
+                    Thread th = new WorkerThread();
+                    workerThreads.add(th);
+                    th.start();
+
+                }
+        );
+    }
+
+    public void schedule(Runnable runnable, long delay) {
+        long timeToRun = System.currentTimeMillis() + delay;
+        taskQueue.add(new Task(timeToRun, runnable));
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+    }
+
+    public void shutDown() {
+        for (Thread workerThread : workerThreads) {
+            workerThread.interrupt();
+        }
+    }
 
     private static class Task implements Comparable<Task> {
         private long timeToRun;
@@ -26,35 +53,27 @@ public class Timer {
     private class WorkerThread extends Thread {
         @Override
         public void run() {
-            while (true) {
-                try {
+            try {
+                while (!interrupted()) {
                     Task timerTask = taskQueue.take();
                     synchronized (lock) {
                         if (System.currentTimeMillis() < timerTask.timeToRun)
-                            lock.wait();
+                            lock.wait(timerTask.timeToRun - System.currentTimeMillis());
                     }
                     if (System.currentTimeMillis() < timerTask.timeToRun)
                         taskQueue.add(timerTask);
                     else
                         timerTask.runnable.run();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
+            } catch (InterruptedException e) {
             }
         }
-
     }
 
-    public Timer(int parallel) {
-        IntStream.range(0, parallel).forEach(x ->
-                new WorkerThread().start()
-        );
-    }
-
-    public void schedule(Runnable runnable, long delay) {
-        //input validation
-        long timeToRun = System.currentTimeMillis() + delay;
-        taskQueue.add(new Task(timeToRun, runnable));
-
+    public static void main(String[] args) throws InterruptedException {
+        Timer timer = new Timer(5);
+        IntStream.range(0, 200).forEach(x -> timer.schedule(() -> System.out.println(x), x));
+        Thread.sleep(10000);
+        timer.shutDown();
     }
 }
